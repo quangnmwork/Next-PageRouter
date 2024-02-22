1f9326ddeba325864a777e2b87492e86d72f129b
quangnmwork <quangnm.work@gmail.com>
2024-02-22 15:59:07 +0900

    feat: i18

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const { i18n } = require("./next-i18next.config.js");
+
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  i18n,
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+};
